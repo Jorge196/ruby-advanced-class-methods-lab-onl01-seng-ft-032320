0aed4a2d780503debda83699dd217cefab1082de
Jorge Ramirez <joramirez96@gmail.com>
2020-04-03 22:06:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,7 +13,7 @@ class Song
   def self.create
     song = self.create
     song.save
-    song 
+    song
   end
 
   def self.new_by_name(name)
@@ -28,18 +28,22 @@ class Song
     song
   end
 
-  def self.find_by_name
+  def self.find_by_name(name)
     @@all.find_by_name{|song|song.name == name}
+    song[0]
   end
 
-  def self.find_or_create_by_name
-    self.find_by_name(name) || self.create_by_name
+  def self.find_or_create_by_name(name)
+    if !find_by_name(name)
+      create_by_name(name)
+    else
+      find_by_name(name)
+    end
   end
 
 
-  def self.alphabetical()
-    @@all.sort_by{|song|song.name}
-
+  def self.alphabetical
+    @@all.sort_by{|song|song.name[0]}
   end
 
   def self.new_from_filename(filename)
