3998c8fde6c853c83c8810020843e720aa482856
Jorge Ramirez <joramirez96@gmail.com>
2020-04-03 21:20:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,12 +7,12 @@ class Song
   end
 
   def self.create
-    song = self.new
+    song = self.create
     song
   end
 
   def self.new_by_name
-    song = self.
+    song = self.new_by_name
 
   def save
     self.class.all << self
